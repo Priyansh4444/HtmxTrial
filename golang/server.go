9eca31b5f4e7d3a96af020e917463691b9b38ef0
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-03-07 01:13:24 -0800

    getting the defautlt configuration of the canvas

--- golang/server.go
+++ golang/server.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/gorilla/sessions"
 )
 
+var store = sessions.NewCookieStore([]byte("something-very-secret"))
+
 func main() {
-	// Define a handler function for the root route ("/")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+	http.HandleFunc("/increment", func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "session-name")
+		if session.Values["counter"] == nil {
+			session.Values["counter"] = 0
+		}
+		session.Values["counter"] = session.Values["counter"].(int) + 1
+		session.Save(r, w)
+		fmt.Fprintf(w, "%d", session.Values["counter"])
+
+		// Print count in CLI
+		fmt.Println("Count:", session.Values["counter"])
 	})
 
-	// Start the server on port 8080
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../public/index.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
