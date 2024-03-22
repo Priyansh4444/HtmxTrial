0749c2d0715501ab7bc3b6301d9bc4d87889d0c2
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-03-21 17:42:07 -0700

    getting air to work

--- golang/server.go
+++ golang/server.go
@@ -10,6 +10,9 @@ import (
 type Templates struct {
 	templates *template.Template
 }
+type Count struct {
+	Count int
+}
 
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
@@ -17,12 +20,22 @@ func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Co
 
 func newTemplate() *Templates {
 	return &Templates{
-		templates: template.Must(template.ParseGlob("public/views/*.html")),
+		templates: template.Must(template.ParseGlob("../public/*.html")),
 	}
 }
 
 func main() {
 	e := echo.New()
+	count := Count{Count: 0}
 	e.Use(middleware.Logger())
+
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "index.html", count)
+	})
+
+	e.Logger.Fatal(e.Start(":42049"))
 
 }
